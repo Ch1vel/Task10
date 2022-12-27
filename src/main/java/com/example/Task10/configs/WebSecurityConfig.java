6cbe15ebb6f81f60bad5810cdba5fc685d5a69ea
Andrey <golubevandrey1992@yandex.ru>
2022-12-27 19:45:40 +0300

    Ver 1.0

--- src/main/java/com/example/Task10/configs/WebSecurityConfig.java
+++ src/main/java/com/example/Task10/configs/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.demo.configs;
+package com.example.Task10.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/example/Task10/services/UserService.java
@@ -0,0 +1,45 @@
+package com.example.Task10.services;
+
+
+import com.example.Task10.models.User;
+import com.example.Task10.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void save(User user){
+        userRepository.save(user);
+    }
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void removeUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+    public User findUser(int id) {
+       return userRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void editUser(User user) {
+        userRepository.save(user);
+    }
+}
